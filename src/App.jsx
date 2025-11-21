a86adf84f32705c00ebbe538f2f4923d6de9dc0b
Salam <salam46.khan@gmail.com>
2025-11-21 20:20:28 +0600

    add alternative

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Alternative from './Components/alternative/Alternative';
 
 const App = () => {
   return (
     <div>
       <p>Hello World</p>
+      <Alternative />
     </div>
   );
 };
